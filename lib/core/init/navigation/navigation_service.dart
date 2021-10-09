fdd028f3c67d04a7cfb4108417c8871c3c0e121e
Selim Doğan <sselimq@gmail.com>
2021-10-10 00:18:59 +0300

    notification manager Fixes #1

--- lib/core/init/navigation/navigation_service.dart
+++ lib/core/init/navigation/navigation_service.dart
@@ -12,12 +12,12 @@ class NavigationService implements INavigationService {
   get removeAllOldRoutes => (Route<dynamic> route) => false;
 
   @override
-  Future<void> navigateToPage(String path, Object data) async {
+  Future<void> navigateToPage(String path, {Object? data}) async {
     await navigatorKey.currentState!.pushNamed(path, arguments: data);
   }
 
   @override
-  Future<void> navigateToPageClear(String path, Object data) async {
+  Future<void> navigateToPageClear(String path, {Object? data}) async {
     await navigatorKey.currentState!
         .pushNamedAndRemoveUntil(path, removeAllOldRoutes, arguments: data);
   }
---
+++ lib/feature/test/view/abc_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ABC extends StatelessWidget {
+  const ABC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
